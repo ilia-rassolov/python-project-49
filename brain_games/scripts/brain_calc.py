4d0b0f4da6fcc799f8969ea0a0841750b8daa928
ilia rassolov <iliarassolov@gmail.com>
2023-02-28 16:22:00 +0400

    calc

---
+++ brain_games/scripts/brain_calc.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+from brain_games.games import calc
+from brain_games.games import logic
+
+
+def main():
+    logic.game_make(calc)
+
+
+if __name__ == '__main__':
+    main()
