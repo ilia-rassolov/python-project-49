b75a73ebc8e52f2edea1ad8cb0eb10c76ed56982
ilia rassolov <iliarassolov@gmail.com>
2023-03-07 16:26:22 +0400

    refactoring_02(!)

--- brain_games/games/logic.py
+++ brain_games/games/logic.py
@@ -13,7 +13,7 @@ def play_game(game):
         if answer != correct_answer:
             print(f"'{answer}' is wrong answer ;(. Correct\
  answer was '{correct_answer}'.")
-            print(f"Let's try again, {name}")
+            print(f"Let's try again, {name}!")
             break
         print('Correct!')
     else:
