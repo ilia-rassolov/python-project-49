51280c4787398401aa79d91d14f341d38e7328bd
ilia rassolov <iliarassolov@gmail.com>
2023-03-22 15:27:21 +0400

    calc.py operator

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -1,15 +1,21 @@
 from random import randint, choice
+from operator import mul, add, sub
 
 
 GAME_CONDITION = 'What is the result of the expression?'
 
 
 def calculation_correct_answer():
-    all_signs = '*+'
+    all_signs = '*+-'
     sign = choice(all_signs)
     number_1 = randint(1, 10)
     number_2 = randint(1, 10)
     question = f'{number_1} {sign} {number_2}'
-    correct_answer = eval(question)
+    if sign == '*':
+        correct_answer = mul(number_1, number_2)
+    elif sign == '+':
+        correct_answer = add(number_1, number_2)
+    else:
+        correct_answer = sub(number_1, number_2)
 
-    return question, str(correct_answer)
+    return (question, str(correct_answer))
