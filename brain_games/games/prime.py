f156d9c6f9cba04c593188d9d69f3ec9d80dac6f
ilia rassolov <iliarassolov@gmail.com>
2023-03-30 13:51:57 +0400

    prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -8,11 +8,15 @@ def calculate_the_correct_answer():
     num = randint(0, 100)
     question = str(num)
     largest_divisor_number = int(num ** 0.5)
-    for i in range(2, largest_divisor_number + 1):
-        if num % i == 0:
-            correct_answer = 'no'
-            break
-        else:
-            correct_answer = 'yes'
 
-    return [question, str(correct_answer)]
+    def is_prime(x):
+        for i in range(2, largest_divisor_number + 1):
+            if x % i == 0:
+                break
+            return x % i == 0
+
+    correct_answer = 'no'
+    if is_prime(num):
+        correct_answer = 'yes'
+
+    return question, correct_answer
