b004c5129eda270cc0a2c057754a141c43a4db84
ilia rassolov <iliarassolov@gmail.com>
2023-04-03 18:35:49 +0400

    prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -7,16 +7,16 @@ GAME_CONDITION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def calculate_the_correct_answer():
     num = randint(0, 100)
     question = str(num)
-    largest_divisor_number = int(num ** 0.5)
 
     def is_prime(x):
+        largest_divisor_number = int(x ** 0.5)
         for i in range(2, largest_divisor_number + 1):
             if x % i == 0:
-                break
-            return x % i == 0
+                return True
+        return False
 
-    correct_answer = 'no'
+    correct_answer = 'yes'
     if is_prime(num):
-        correct_answer = 'yes'
+        correct_answer = 'no'
 
     return question, correct_answer
