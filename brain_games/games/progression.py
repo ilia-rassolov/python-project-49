4f90a81db59d3b9f0218103357a684193a3cdd5d
ilia rassolov <iliarassolov@gmail.com>
2023-04-03 18:38:35 +0400

    progression.py

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,16 +5,19 @@ GAME_CONDITION = 'What number is missing in the progression?'
 
 
 def calculate_the_correct_answer():
-    first_in_sequence = randint(0, 10)
-    step_sequence = randint(1, 10)
     unknown_sequence_number = randint(0, 9)
-    sequence = []
-    for i in range(10):
-        sequence.append(first_in_sequence + i * step_sequence)
-    correct_answer = sequence[unknown_sequence_number]
-    sequence.pop(unknown_sequence_number)
-    sequence_string = map(str, sequence)
-    sequence.insert(unknown_sequence_number, '..')
+
+    def generate_progression():
+        first_in_sequence = randint(0, 10)
+        step_sequence = randint(1, 10)
+        sequence = []
+        for i in range(10):
+            sequence.append(str(first_in_sequence + i * step_sequence))
+        return sequence
+
+    sequence_string = generate_progression()
+    correct_answer = sequence_string[unknown_sequence_number]
+    sequence_string[unknown_sequence_number] = '..'
     question = ' '.join(sequence_string)
 
     return question, str(correct_answer)
